7cb295699537f24f388bdbd97288800fe90a99ad
Kenny Udovic <kennyudovic@gmail.com>
2025-10-19 10:36:36 -0400

    chore: remove dead code

--- lua/plugins/ai-assistants.lua
+++ lua/plugins/ai-assistants.lua
@@ -1,99 +1,47 @@
 return {
   -- GitHub Copilot
-  -- { "github/copilot.vim", event = "InsertEnter" },
-
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   opts = {
-  --     suggestion = {
-  --       enabled = not vim.g.ai_cmp,
-  --       auto_trigger = true,
-  --       hide_during_completion = vim.g.ai_cmp,
-  --       keymap = {
-  --         accept = false, -- handled by nvim-cmp / blink.cmp
-  --         next = "<M-]>",
-  --         prev = "<M-[>",
-  --       },
-  --     },
-  --     panel = { enabled = false },
-  --     filetypes = {
-  --       markdown = true,
-  --       help = true,
-  --     },
-  --   },
-  -- },
-
-
-
-----
-  --{
-  --  "zbirenbaum/copilot.lua",
-  --  opts = {
-  --    suggestion = {
-  --      enabled = true,
-  --      auto_trigger = true,
-  --      hide_during_completion = vim.g.ai_cmp,
-  --      keymap = {
-  --        accept = false, -- handled by nvim-cmp / blink.cmp
-  --        next = "<M-]>",
-  --        prev = "<M-[>",
-  --      },
-  --    },
-  --    panel = {
-  --      enabled = true,
-  --    },
-  --    -- filetypes = {
-  --    --   markdown = true,
-  --    --   help = true,
-  --    -- },
-  --  },
-  --},
----
-
-{
-  "zbirenbaum/copilot.lua",
-  opts = {
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      debounce = 300, -- 👈 custom field we’ll use below
-      keymap = {
-        accept = false,
-        next = "<M-]>",
-        prev = "<M-[>",
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        debounce = 300, -- 👈 custom field we’ll use below
+        keymap = {
+          accept = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
       },
+      panel = { enabled = true },
     },
-    panel = { enabled = true },
-  },
-  config = function(_, opts)
-    local copilot = require("copilot")
-    local suggestion = require("copilot.suggestion")
+    config = function(_, opts)
+      local copilot = require("copilot")
+      local suggestion = require("copilot.suggestion")
 
-    copilot.setup(opts)
+      copilot.setup(opts)
 
-    -- 👇 Debounce logic
-    if opts.suggestion and opts.suggestion.auto_trigger and opts.suggestion.debounce then
-      local timer
-      vim.api.nvim_create_autocmd("TextChangedI", {
-        callback = function()
-          if timer then
-            timer:stop()
-            timer:close()
-          end
-          timer = vim.uv.new_timer()
-          timer:start(opts.suggestion.debounce, 0, function()
-            vim.schedule(function()
-              suggestion.next()
+      -- 👇 Debounce logic
+      if opts.suggestion and opts.suggestion.auto_trigger and opts.suggestion.debounce then
+        local timer
+        vim.api.nvim_create_autocmd("TextChangedI", {
+          callback = function()
+            if timer then
+              timer:stop()
+              timer:close()
+            end
+            timer = vim.uv.new_timer()
+            timer:start(opts.suggestion.debounce, 0, function()
+              vim.schedule(function()
+                suggestion.next()
+              end)
             end)
-          end)
-        end,
-      })
-    end
-  end,
-},
-
-
+          end,
+        })
+      end
+    end,
+  },
 
   -- Avante.nvim for other AI providers
   {
